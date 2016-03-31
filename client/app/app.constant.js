511daac57c0d834c20ad471e3f5750914a72ddda
Duong Tam Chau <dtc@iuservice.com>
2016-03-31 15:26:17 -0400

    Initial commit

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("theSharksApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('theSharksApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });

ceab9a68e91dc6e670794d0e97e21120eb81a834
Dongfeng Gu <gudongfeng@outlook.com>
2016-04-01 13:06:25 -0400

    add the oembed format of the tweet

--- server/api/twitter/twitter.controller.js
+++ server/api/twitter/twitter.controller.js
@@ -112,10 +112,45 @@ export function destroy(req, res) {
 
 // Return the twitter hashtag information
 export function search(req, res) {
+
+  var oEmbedTweets = [], tweets = [];
+
   client.get('search/tweets', {q: '#' + req.params.value, count: 10},
-      function (error, tweets, response) {
-        res.setHeader('Content-Type', 'application/json');
-        return res.send(JSON.stringify(tweets.statuses, null, 3));
+      function (error, data, response) {
+
+        tweets = data.statuses;
+        //console.log(tweets);
+
+        var i = 0, len = tweets.length;
+
+        for(i; i < len; i++) {
+          getOEmbed(tweets[i]);
+        }
       }
   );
+
+  function getOEmbed(tweet){
+
+    // oEmbed request params
+    var params = {
+      "id": tweet.id_str,
+      "maxwidth": 305,
+      "hide_thread": true,
+      "omit_script": true
+    };
+
+    // request data
+    client.get('statuses/oembed', params, function (err, data, resp) {
+      tweet.oEmbed = data;
+      oEmbedTweets.push(tweet);
+
+      // do we have oEmbed HTML for all Tweets?
+      if (oEmbedTweets.length == tweets.length) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(oEmbedTweets);
+      }
+    });
+
+  }
+
 }

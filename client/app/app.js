e2c26dbd7f52119f9ca3436875c1a9ddd518dec8
Duong Tam Chau <dtc@iuservice.com>
2016-06-10 21:51:01 -0400

    Integrate angularfire into our projects.

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('theSharksApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'firebase'
 ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider

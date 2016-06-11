fccc4b037015bb2b48f33955c3e38b15eaf0f92f
Duong Tam Chau <dtc@iuservice.com>
2016-06-10 22:23:39 -0400

    Scaffolding question service.

---
+++ client/app/questionService/questionService.service.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('theSharksApp.questionService')
+  .service('questionService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });

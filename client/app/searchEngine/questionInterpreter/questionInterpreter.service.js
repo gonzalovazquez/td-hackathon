198be86ef39a2b052afab89abbe82111d21797d8
gonzalovazquez <gonzalo.segura1@gmail.com>
2016-06-11 10:17:05 -0400

    questions feed into questionIntepreter

--- client/app/searchEngine/questionInterpreter/questionInterpreter.service.js
+++ client/app/searchEngine/questionInterpreter/questionInterpreter.service.js
@@ -3,6 +3,7 @@
 angular.module('theSharksApp.searchEngine')
   .service('questionInterpreter', function(answerMatcher) {
     this.interpret = (question) => {
+      console.log(question);
       return answerMatcher.match(question.trim().split(/ +/).map(k => k.toLowerCase()));
     };
   });
